0afd08e26f408756a3a991807a7992dedf99ddbd
Arnulfo trabajo <arnulfo.aguilar9405@gmail.com>
2018-10-26 15:36:16 -0600

    Cargardatos

---
+++ Sistema de Embalaje Kit de Transmision/Sistema de Embalaje Kit de Transmision/Cargar/Cargar.vb
@@ -0,0 +1,3 @@
+﻿Public Class Cargar
+
+End Class
--- Sistema de Embalaje Kit de Transmision/Sistema de Embalaje Kit de Transmision/Conexion.vb
+++ Sistema de Embalaje Kit de Transmision/Sistema de Embalaje Kit de Transmision/Conexion.vb
@@ -1,7 +1,7 @@
 ﻿Imports System.Data.OracleClient
 
 Module Conexion
-    Dim cadena As String = "Data Source=172.16.1.16:1521; User id=EMBALAJE; password=embalaje01"
+    Dim cadena As String = "Data Source=172.16.1.16:1521/RFID; User id=EMBALAJE; password=embalaje01"
     Dim cadena2 As String = "Data Source=LOCALHOST:1521; User id=Despacho01; password=marmol01"
     Public con As New OracleConnection(cadena)
     Public con2 As New OracleConnection(cadena2)
