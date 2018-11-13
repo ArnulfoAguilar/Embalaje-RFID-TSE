3b7697ff8502f4d04e845a8321961973b9d4b4b1
JoshGM <marmoljg@hotmail.com>
2018-11-13 08:30:36 -0600

    para que corra

--- Sistema de Embalaje Kit de Transmision/Sistema de Embalaje Kit de Transmision/Conexion.vb
+++ Sistema de Embalaje Kit de Transmision/Sistema de Embalaje Kit de Transmision/Conexion.vb
@@ -9,7 +9,8 @@ Module Conexion
     Public con2 As New OracleConnection(cadena)
 
     Public num As Integer
-    Public user As String
+    Public user_global As Integer
+    Public rol_global As Integer
 End Module
 
 
